a337dd4f1b5677259a45aad3c65c5dde4c75bafc
luisaGonzales <luisagc711@gmail.com>
2017-10-26 03:08:00 -0500

    Store funciona

--- src/Store.js
+++ src/Store.js
@@ -1,22 +1,7 @@
 import createStore from 'redux-zero';
 
-let comments = [
-    {
-        userName: "Luisa Gonzales",
-        userComment: "Excellent content"
-    },
-    {
-        userName: "Kely Vargaya",
-        userComment: "I like this page"
-    },
-    {
-        userName: "Alejandra Adrián",
-        userComment: "Like this ..."
-    }
-];
-
 const initialState = {
-    comments: comments,
+    comments: [],
 }
 
 const store = createStore(initialState);
